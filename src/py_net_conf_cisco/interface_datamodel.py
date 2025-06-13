c5d06aeeb8070bae91587dc3995cf2f5f7536c01
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-06-13 16:55:51 -0400

    Renamed file becasue it was getting long. Completed the initial amount of interface options.

---
+++ src/py_net_conf_cisco/interface_datamodel.py
@@ -0,0 +1,67 @@
+"""
+Data Models fo configuration options
+
+This module provides dataclasses for structuring configuration options.
+"""
+
+from dataclasses import dataclass
+from enum import Enum
+from ipaddress import IPv4Interface
+from typing import Optional
+
+
+class InterfaceType(Enum):
+    """Allowabld interface types"""
+
+    ETHERNET = "Ethernet"
+    GIGABITETHERNET = "GigabitEthernet"
+    TENGIGABITETHERNET = "TenGigabitEthernet"
+    TWENTYFIVEGIGABITETHERNET = "TwentyFiveGigE"
+    FORTYGIGABITETHERNET = "FortyGigabitEthernet"
+    HUNDREDGIGABITETHERNET = "HundredGigE"
+    VLAN = "Vlan"
+    LOOPBACK = "loopback"
+    PORT_CHANNEL = "port-channel"
+    NVE = "nve"
+
+
+@dataclass
+class InterfaceConfig:
+    """Represents an interface configuration."""
+
+    interface_type: InterfaceType
+    interface_number: str
+    subinterface_number: Optional[int] = None
+    ip_address: Optional[IPv4Interface] = None
+    vrf: Optional[str] = None
+    dhcp_assigned: Optional[bool] = None
+    description: Optional[str] = None
+    shutdown: Optional[str] = None
+
+    def __post_init__(self):
+        if any(
+            [
+                self.dhcp_assigned is True and self.ip_address is not None,
+                self.dhcp_assigned is False and self.ip_address is None,
+            ]
+        ):
+            raise Exception
+
+    def to_config_lines(self):
+        lines = [
+            f"interface {self.interface_type.value}{self.interface_number}{'' if self.subinterface_number is None else '.' + str(self.subinterface_number)}"
+        ]
+        if self.description is not None:
+            lines.append(f" description {self.description}")
+        if self.vrf is not None:
+            lines.append(f" vrf forwarding {self.vrf}")
+        if self.ip_address is not None:
+            lines.append(
+                f" ip address {str(self.ip_address.ip)} {str(self.ip_address.netmask)}"
+            )
+        elif self.dhcp_assigned:
+            lines.append(" ip address dhcp")
+        if self.shutdown is not None:
+            lines.append(" shutdown")
+        lines.append("!")
+        return lines
---
+++ tests/test_datamodels.py
@@ -0,0 +1,169 @@
+"""
+Test suite for the interface datamodels.
+"""
+
+import ipaddress
+from ipaddress import IPv4Interface
+
+from pytest import raises
+
+from py_net_conf_cisco.interface_datamodel import InterfaceConfig, InterfaceType
+
+
+class TestInterfaceConfig:
+    """Test class for the InterfaceConfig dataclass."""
+
+    def test_empty_creation_throws_excptions(self):
+        """Test a empty creation fails"""
+        with raises(TypeError):
+            interface = InterfaceConfig()
+            return interface
+
+    def test_creation_with_no_interface_type_throws_exception(self):
+        """Test a creation with no InterfaceType failes"""
+        with raises(TypeError):
+            interface = InterfaceConfig(interface_number="1")
+            return interface
+
+    def test_creation_with_no_number_type_throws_exception(self):
+        """Test a creation with no InterfaceType failes"""
+        with raises(TypeError):
+            interface = InterfaceConfig(InterfaceType.ETHERNET)
+            return interface
+
+    def test_basic_creation(self):
+        """Test with only an InterfaceType"""
+        interface = InterfaceConfig(
+            InterfaceType.ETHERNET, interface_number="1"
+        )
+        assert interface is not None
+        assert interface.ip_address is None
+        assert interface.vrf is None
+        assert interface.description is None
+        assert interface.shutdown is None
+
+    def test_interface_with_ip(self):
+        """Test interface creation with IP address."""
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            ip_address=IPv4Interface("1.1.1.1/24"),
+        )
+        assert interface.interface_type == InterfaceType.ETHERNET
+        assert interface.ip_address == IPv4Interface("1.1.1.1/24")
+        assert interface.dhcp_assigned is not True
+
+    def test_interface_with_dhcp(self):
+        """Test the IP address can be set with DHCP"""
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            dhcp_assigned=True,
+        )
+        assert interface.interface_type == InterfaceType.ETHERNET
+        assert interface.ip_address is None
+        assert interface.dhcp_assigned
+
+    def test_interface_with_dhcp_and_ip_fails(self):
+        """Test the IP address and DHCP fails"""
+        with raises(Exception):
+            interface = InterfaceConfig(
+                interface_type=InterfaceType.ETHERNET,
+                interface_number="1",
+                dhcp_assigned=True,
+                ip_address=IPv4Interface("1.1.1.1/24"),
+            )
+            return interface
+
+    def test_interface_with_false_dhcp_and_no_ip_fails(self):
+        """Test the IP address and DHCP fails"""
+        with raises(Exception):
+            interface = InterfaceConfig(
+                interface_type=InterfaceType.ETHERNET,
+                interface_number="1",
+                dhcp_assigned=False,
+            )
+            return interface
+
+    def test_to_config_lines_minimal(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET, interface_number=1
+        )
+        expected_lines = ["interface Ethernet1", "!"]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_subinterface(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number=1,
+            subinterface_number=2,
+        )
+        expected_lines = ["interface Ethernet1.2", "!"]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_description(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number=1,
+            description="Test description",
+        )
+        expected_lines = [
+            "interface Ethernet1",
+            " description Test description",
+            "!",
+        ]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_ip_address(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            ip_address=ipaddress.IPv4Interface("1.1.1.1/24"),
+        )
+        expected_lines = [
+            "interface Ethernet1",
+            " ip address 1.1.1.1 255.255.255.0",
+            "!",
+        ]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_vrf_and_ip_address(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            ip_address=ipaddress.IPv4Interface("1.1.1.1/24"),
+            vrf="test",
+        )
+        expected_lines = [
+            "interface Ethernet1",
+            " vrf forwarding test",
+            " ip address 1.1.1.1 255.255.255.0",
+            "!",
+        ]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_dhcp_address(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            dhcp_assigned=True,
+        )
+        expected_lines = [
+            "interface Ethernet1",
+            " ip address dhcp",
+            "!",
+        ]
+        assert expected_lines == interface.to_config_lines()
+
+    def test_to_config_lines_with_shutdown(self):
+        interface = InterfaceConfig(
+            interface_type=InterfaceType.ETHERNET,
+            interface_number="1",
+            shutdown=True,
+        )
+        expected_lines = [
+            "interface Ethernet1",
+            " shutdown",
+            "!",
+        ]
+        assert expected_lines == interface.to_config_lines()
