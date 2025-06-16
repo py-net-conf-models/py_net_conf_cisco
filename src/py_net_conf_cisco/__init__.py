bb82c03091999de0fe57cd6c0dc13e1c5371e69c
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-06-16 15:56:43 -0400

    Changed the filename to match the class name.

--- src/py_net_conf_cisco/__init__.py
+++ src/py_net_conf_cisco/__init__.py
@@ -1,6 +1,6 @@
 # from .interface_datamodel import InterfaceConfig
 
-from .ciscoconf import CiscoConfig
+from .ciscoconfig import CiscoConfig
 from .interface_datamodel import InterfaceConfig
 
 __version__ = "0.1.0"
