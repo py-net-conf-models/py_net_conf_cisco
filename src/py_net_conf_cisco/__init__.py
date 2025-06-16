0a728f797a9a20af583bf01ba634a49a0a5f4340
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-06-16 14:26:04 -0400

    Built test for the module. Updated __init.py__ to have a version and easy to import classes.

--- src/py_net_conf_cisco/__init__.py
+++ src/py_net_conf_cisco/__init__.py
@@ -1,2 +1,12 @@
+# from .interface_datamodel import InterfaceConfig
+
+from .interface_datamodel import InterfaceConfig
+
+__version__ = "0.1.0"
+__all__ = [
+    "InterfaceConfig",
+]
+
+
 def hello() -> str:
     return "Hello from py-net-conf-cisco!"
