24906c4040e16d4e3d13d719eff83f53d5970964
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-06-16 10:36:34 -0400

    Change to importing pytest due to issues with getting pytest.mark.parametrize imported with from. Moved to using a paramaterized function for testing to_string().

--- tests/test_datamodels.py
+++ tests/test_datamodels.py
@@ -5,7 +5,7 @@ Test suite for the interface datamodels.
 import ipaddress
 from ipaddress import IPv4Interface
 
-from pytest import raises
+import pytest
 
 from py_net_conf_cisco.interface_datamodel import InterfaceConfig, InterfaceType
 
@@ -15,19 +15,19 @@ class TestInterfaceConfig:
 
     def test_empty_creation_throws_excptions(self):
         """Test a empty creation fails"""
-        with raises(TypeError):
+        with pytest.raises(TypeError):
             interface = InterfaceConfig()  # pyright: ignore
             return interface
 
     def test_creation_with_no_interface_type_throws_exception(self):
         """Test a creation with no InterfaceType failes"""
-        with raises(TypeError):
+        with pytest.raises(TypeError):
             interface = InterfaceConfig(interface_number="1")  # pyright: ignore
             return interface
 
     def test_creation_with_no_number_type_throws_exception(self):
         """Test a creation with no InterfaceType failes"""
-        with raises(TypeError):
+        with pytest.raises(TypeError):
             interface = InterfaceConfig(InterfaceType.ETHERNET)  # pyright: ignore
             return interface
 
@@ -66,7 +66,7 @@ class TestInterfaceConfig:
 
     def test_interface_with_dhcp_and_ip_fails(self):
         """Test the IP address and DHCP fails"""
-        with raises(Exception):
+        with pytest.raises(Exception):
             interface = InterfaceConfig(
                 interface_type=InterfaceType.ETHERNET,
                 interface_number="1",
@@ -77,7 +77,7 @@ class TestInterfaceConfig:
 
     def test_interface_with_false_dhcp_and_no_ip_fails(self):
         """Test the IP address and DHCP fails"""
-        with raises(Exception):
+        with pytest.raises(Exception):
             interface = InterfaceConfig(
                 interface_type=InterfaceType.ETHERNET,
                 interface_number="1",
@@ -85,86 +85,72 @@ class TestInterfaceConfig:
             )
             return interface
 
-    def test_to_config_lines_minimal(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number=1,  # pyright: ignore
-        )
-        expected_lines = ["interface Ethernet1", "!"]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_subinterface(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            subinterface_number=2,
-        )
-        expected_lines = ["interface Ethernet1.2", "!"]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_description(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            description="Test description",
-        )
-        expected_lines = [
-            "interface Ethernet1",
-            " description Test description",
-            "!",
-        ]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_ip_address(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            ip_address=ipaddress.IPv4Interface("1.1.1.1/24"),
-        )
-        expected_lines = [
-            "interface Ethernet1",
-            " ip address 1.1.1.1 255.255.255.0",
-            "!",
-        ]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_vrf_and_ip_address(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            ip_address=ipaddress.IPv4Interface("1.1.1.1/24"),
-            vrf="test",
-        )
-        expected_lines = [
-            "interface Ethernet1",
-            " vrf forwarding test",
-            " ip address 1.1.1.1 255.255.255.0",
-            "!",
-        ]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_dhcp_address(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            dhcp_assigned=True,
-        )
-        expected_lines = [
-            "interface Ethernet1",
-            " ip address dhcp",
-            "!",
-        ]
-        assert expected_lines == interface.to_config_lines()
-
-    def test_to_config_lines_with_shutdown(self):
-        interface = InterfaceConfig(
-            interface_type=InterfaceType.ETHERNET,
-            interface_number="1",
-            shutdown=True,
-        )
-        expected_lines = [
-            "interface Ethernet1",
-            " shutdown",
-            "!",
-        ]
-        assert expected_lines == interface.to_config_lines()
+    @pytest.mark.parametrize(
+        "kwargs,expected_lines",
+        [
+            (  # Test with minimal configuration
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": "1",
+                },
+                ["interface Ethernet1", "!"],
+            ),
+            (  # Test with an IP address
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": "1",
+                    "ip_address": ipaddress.IPv4Interface("192.168.1.1/24"),
+                },
+                [
+                    "interface Ethernet1",
+                    " ip address 192.168.1.1 255.255.255.0",
+                    "!",
+                ],
+            ),
+            (  # Test with an IP address and a VRF)
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": "1",
+                    "ip_address": ipaddress.IPv4Interface("1.1.1.1/24"),
+                    "vrf": "test",
+                },
+                [
+                    "interface Ethernet1",
+                    " vrf forwarding test",
+                    " ip address 1.1.1.1 255.255.255.0",
+                    "!",
+                ],
+            ),
+            (  # Test with DHCP address
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": "1",
+                    "dhcp_assigned": True,
+                },
+                [
+                    "interface Ethernet1",
+                    " ip address dhcp",
+                    "!",
+                ],
+            ),
+            (  # Test with a description
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": 1,
+                    "description": "Test description",
+                },
+                ["interface Ethernet1", " description Test description", "!"],
+            ),
+            (  # Test with shutdown
+                {
+                    "interface_type": InterfaceType.ETHERNET,
+                    "interface_number": "1",
+                    "shutdown": True,
+                },
+                ["interface Ethernet1", " shutdown", "!"],
+            ),
+        ],
+    )
+    def test_to_config_lines_various(self, kwargs, expected_lines):
+        interface = InterfaceConfig(**kwargs)
+        assert interface.to_config_lines() == expected_lines
