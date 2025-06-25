192d3cbd0881de8c57aaa0518b56f035151a3231
Troy Holder <964446+troyholder@users.noreply.github.com>
2025-06-25 08:33:44 -0400

    Need to import InterfaceType since it is listed as an export.

--- src/py_net_conf_cisco/__init__.py
+++ src/py_net_conf_cisco/__init__.py
@@ -1,5 +1,5 @@
 from .ciscoconfig import CiscoConfig
-from .interface_datamodel import InterfaceConfig
+from .interface_datamodel import InterfaceConfig, InterfaceType
 
 __version__ = "0.1.0"
 __all__ = ["InterfaceConfig", "CiscoConfig", "InterfaceType"]
